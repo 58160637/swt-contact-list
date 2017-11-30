7a03b1db23ee31e02965cff2adaf80ae510b68be
58160637 <58160637@go.buu.ac.th>
2017-11-30 16:46:26 +0700

    test delete than respond 204

--- test/contact-list.spec.js
+++ test/contact-list.spec.js
@@ -4,3 +4,23 @@ const app = require('../server');
 test('simple test',()=>{
   expect(1).toEqual(1);
 })
+
+
+
+
+
+
+
+
+describe('DELETE /contacts/:id',()=>{
+  it('Should return 204',(done)=>{
+    request(app).delete('/contacts/:11')
+      .set('Accept','application/json')
+      .expect(204)
+      .then((res)=>{
+        //let contact = res.body
+        //expect(contact.length).toBe(11)
+        done()
+      })
+  })
+})
