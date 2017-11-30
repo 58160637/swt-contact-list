b0572d16a401dd3dc2097a45019eee04d2c59807
58160637 <58160637@go.buu.ac.th>
2017-11-30 17:43:55 +0700

    modified test post

--- test/contact-list.spec.js
+++ test/contact-list.spec.js
@@ -38,13 +38,19 @@ test('simple test',()=>{
 describe('POST /contacts',()=>{
   it('Should return 201',(done)=>{
     request(app).post('/contacts')
-      .send({name : 'Waramporn Yangkijkarn',email : "58160637@go.buu.ac.th", phone : "123-456-7890",url :"www.github.com/5816063",notes:"final make me wanna die"})
+      .send({name : 'Waramporn Yangkijkarn',email : "58160637@go.buu.ac.th", phone : "123-456-7890",url :"www.github.com/58160637",notes:"final make me wanna die"})
       .set('Accept','application/json')
       .expect(201)
       .then((res)=>{
-        //let contacts = res.body
-        //expect(contacts[0]).toBeDefined()
-        //expect(contacts[11].name).toBe('Waramporn')
+        let contact = res.body
+        expect(contact.id).toBeDefined()
+        //expect(contact.id).toBe(12)
+        expect(contact.name).toBe('Waramporn Yangkijkarn')
+        expect(contact.email).toBe('58160637@go.buu.ac.th')
+        expect(contact.phone).toBe('123-456-7890')
+        expect(contact.url).toBe('www.github.com/58160637')
+        expect(contact.notes).toBe('final make me wanna die')
+
         done()
       })
   })
